d5961b02eef8a44eb1367ba3758cf5b01c60c2d8
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2023-06-06 18:29:26 +0530

    everything working well let's work

---
+++ index.js
@@ -0,0 +1,31 @@
+//express js
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+//setup
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+const PORT = process.env.PORT || 5000;
+console.log("port is : "+PORT);
+
+
+app.get("/",(req,res)=>{
+    res.send("hello ji");
+});
+
+app.get("/hey",(req,res)=>{
+    res.send("noii noii");
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server is running at https://localhost"+PORT);
+})
